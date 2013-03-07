6010c5e44776ca553146519ef6646fcecd6ac610
Lasse Dissing Hansen <volpes@gmx.com>
2013-03-07 21:09:32 +0100

    Added a lot of classes required for implementing all the new managers. Goona be great :D

---
+++ src/volpes/ldk/client/ProcessManager.java
@@ -0,0 +1,44 @@
+package volpes.ldk.client;
+
+import java.util.ArrayList;
+
+/**
+ * @author Lasse Dissing Hansen
+ */
+public class ProcessManager {
+
+    private ArrayList<Process> processList = new ArrayList<Process>();
+
+
+    void attach(Process process) {
+        processList.add(process);
+    }
+
+    void detach(Process process) {
+        processList.remove(process);
+    }
+
+    void tickProcesses() {
+        for (Process process : processList) {
+            if (process.getState() != Process.PAUSED) {
+                if (process.getState() != Process.DYING) {
+                    process.tick();
+                } else {
+                    if (process.hasChild()){
+                        attach(process.getChild());
+                    }
+                    detach(process);
+                }
+            }
+        }
+
+    }
+
+    Process getProcess(int processId) {
+        for (Process process : processList) {
+            return process;
+        }
+        return null;
+    }
+
+}
